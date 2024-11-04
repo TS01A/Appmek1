3ea68f45ae4921d51cc50ca3a0e56c4539bb5854
AbdullahEesa1 <skl_7@hotmail.com>
2024-11-04 16:48:57 +0300

    Initial Commit

---
+++ Appmek1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Appmek1
+//
+//  Created by Abdullah on 02/05/1446 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
